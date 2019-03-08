974deb086c9c75fe4edef6078639a08fb5af294b
hendisantika <hendisantika@yahoo.co.id>
2019-03-08 18:59:13 +0700

    #3 - Add Role Entity

---
+++ src/main/java/com/hendisantika/registration/entity/Role.java
@@ -0,0 +1,41 @@
+package com.hendisantika.registration.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-account-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-08
+ * Time: 18:58
+ */
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "role")
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
