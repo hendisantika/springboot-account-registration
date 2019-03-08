d0c41ccea4ee2b386cf3606ceea68f8092375eb8
hendisantika <hendisantika@yahoo.co.id>
2019-03-08 18:52:30 +0700

    #1 - Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-account-registration
---
+++ src/main/java/com/hendisantika/registration/SpringbootAccountRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAccountRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAccountRegistrationApplication.class, args);
+    }
+
+}
