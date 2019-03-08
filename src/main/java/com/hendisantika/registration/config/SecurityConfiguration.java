2f217af0995dcb7e0877c6952440bb70209ced49
hendisantika <hendisantika@yahoo.co.id>
2019-03-08 20:19:11 +0700

    #19 - Bug Fix Security Configuration

--- src/main/java/com/hendisantika/registration/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/registration/config/SecurityConfiguration.java
@@ -27,8 +27,8 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final String USERS_QUERY = "select email, password, active from user where email=?";
-    private final String ROLES_QUERY = "select u.email, r.role from user u inner join user_role ur on (u.id = ur.user_id) inner join role r on (ur.role_id=r.role_id) where u.email=?";
+    private final String USERS_QUERY = "select email, password, active from users where email=?";
+    private final String ROLES_QUERY = "select u.email, r.role from users u inner join user_role ur on (u.id = ur.user_id) inner join role r on (ur.role_id=r.role_id) where u.email=?";
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
